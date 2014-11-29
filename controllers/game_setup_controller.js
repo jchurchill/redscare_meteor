8533f02163f0edba30ee288d823af01666c25b05
jchurchill <justin.e.churchill@gmail.com>
2014-11-29 15:59:20 -0500

    Pushing logic that hits the db directly into services

--- controllers/game_setup_controller.js
+++ controllers/game_setup_controller.js
@@ -1,71 +1,46 @@
 Meteor.startup(function() {
 // Usings
 var Games = RedScare.Collections.Games;
+var Status = RedScare.Constants.gameStatus;
+var GameStateManager = RedScare.Services.GameStateManager;
 var Controller = MeteorController.namespace("game_setup");
 
 Controller.methods({
 	abandon: function(gameId) {
 		_private.throwIfNotLoggedIn();
-		var game = Games.findOne(gameId, {
-			fields: { creator: 1 }
-		});
-		if (!game) {
-			throw new Meteor.Error(403, "Game not found");
-		}
-		Games.update({
-			_id: gameId,
-			// Only the creator of the game is allowed to abandon it
-			creator: Meteor.userId()
-		}, {
-			$set: {
-				dateAbandoned: new Date(),
-				status: RedScare.Constants.gameStatus.abandoned
-			}
-		});
+		GameStateManager.abandonGame(gameId, Meteor.userId());
 	},
 
 	addPlayer: function(gameId, userId) {
+		var afterAddPlayer, afterGameMarkedReady;
+
 		_private.throwIfNotLoggedIn();
 		_private.throwIfNotCurrentUser(userId);
 
-		Games.update({
-			_id: gameId,
-			// Do not add a player to a game they're already in
-			players: { $not: { $in: [userId] } },
-			// Do not add a player to a game that is at full capacity
-			$where: "this.players.length < this.playerCount",
-			// Players cannot be added to an in-progress game
-			status: RedScare.Constants.gameStatus.waitingForPlayers
-		}, {
-			$push: { players: userId }
-		},
-		function(err, updated) {
-			if (err || updated === 0) { return; }
-			_private.tryMarkGameAsReady(gameId, /* successCallback: */ function() {
-				// Wait some amount of time, then transition state to begin game
-				if (Meteor.isServer) {
-					Meteor.setTimeout(function() {
-						_private.beginGame(gameId);
-					}, _private.countdownToGameStartMs);
-				}
-			});
-		});
+		// After adding a player to the game, mark the game as "Ready to begin"
+		// if conditions correct (room is full, etc)
+		afterAddPlayer = function() {
+			GameStateManager.tryMarkGameAsReady(gameId, afterGameMarkedReady);
+		};
+
+		// After the game is marked as ready, wait some amount of time,
+		// then transition state to begin game
+		afterGameMarkedReady = function() {
+			if (Meteor.isServer) {
+				Meteor.setTimeout(function() {
+					GameStateManager.beginGame(gameId);
+				}, _private.countdownToGameStartMs);
+			}
+		};
+
+		GameStateManager.addPlayerToGame(gameId, userId, afterAddPlayer);
 	},
 
 	removePlayer: function(gameId, userId) {
 		_private.throwIfNotLoggedIn();
 		_private.throwIfNotCurrentUser(userId);
 
-		Games.update({
-			_id: gameId,
-			// The creator of a game cannot leave it
-			creator: { $ne: userId },
-			// Players cannot leave an in-progress game
-			status: RedScare.Constants.gameStatus.waitingForPlayers
-		}, {
-			$pull: { players: userId },
-			$unset: { dateReadyToBegin: '' }
-		});
+		GameStateManager.removePlayerFromGame(gameId, userId);
 	}
 });
 
@@ -73,43 +48,17 @@ Controller.methods({
 // Private methods referenced above //
 //////////////////////////////////////
 var _private = {};
+
 // TODO: if front end wants access to this to setup some kind
 // TODO: of countdown, make this more accessible
 _private.countdownToGameStartMs = 5000;
-_private.tryMarkGameAsReady = function(gameId, successCallback) {
-	Games.update({
-		_id: gameId,
-		// Mark as ready only if status is still waitingForPlayers
-		// and the game is at full capacity
-		status: RedScare.Constants.gameStatus.waitingForPlayers,
-		$where: "this.players.length === this.playerCount"
-	}, {
-		$set: { dateReadyToBegin: new Date() }
-	}, function(err, updated) {
-		if (err || updated === 0) { return; }
-		(successCallback || function(){}).call(null);
-	});
-};
-_private.beginGame = function(gameId) {
-	Games.update({
-		_id: gameId,
-		// Begin only if status is still waitingForPlayers
-		// and the game is at full capacity,
-		// and the dateReadyToBegin is set
-		status: RedScare.Constants.gameStatus.waitingForPlayers,
-		$where: "this.players.length === this.playerCount",
-		dateReadyToBegin: { $exists: true }
-	}, {
-		$set: {
-			status: RedScare.Constants.gameStatus.nominating
-		}
-	});
-};
+
 _private.throwIfNotLoggedIn = function() {
 	if (!Meteor.userId()) {
 		throw new Meteor.Error(403, "Must be logged in to do this operation");
 	}
 };
+
 _private.throwIfNotCurrentUser = function(userId) {
 	if (userId !== Meteor.userId()) {
 		throw new Meteor.Error(403, "The user is not permissioned to do this operation.");
--- services/game_state_manager.js
+++ services/game_state_manager.js
@@ -1,18 +1,73 @@
+RedScare.NamespaceManager.define("Services.GameStateManager");
+
 Meteor.startup(function() {
 // Usings
+var GameStateManager = RedScare.Services.GameStateManager;
 var Games = RedScare.Collections.Games;
 var Status = RedScare.Constants.gameStatus;
 var Presets = RedScare.Constants.presets;
 
 //////////////////////////////////////////////
-// Public service methods 
+// Public methods 
 /////////////////////////////////////////////
-RedScare.NamespaceManager.define("Services", {
-	beginGame: beginGame,
-	setupNewRound: setupNewRound
-});
 
-function beginGame(gameId, successCallback) {
+GameStateManager.abandonGame = function(gameId, userId, successCallback) {
+	Games.update({
+		_id: gameId,
+		// Only the creator of the game is allowed to abandon it
+		creator: userId
+	}, {
+		$set: {
+			dateAbandoned: new Date(),
+			status: Status.abandoned
+		}
+	},
+	callbackIfSuccessful(successCallback));
+};
+
+GameStateManager.addPlayerToGame = function(gameId, userId, successCallback) {
+	Games.update({
+		_id: gameId,
+		// Do not add a player to a game they're already in
+		players: { $not: { $in: [userId] } },
+		// Do not add a player to a game that is at full capacity
+		$where: "this.players.length < this.playerCount",
+		// Players cannot be added to an in-progress game
+		status: Status.waitingForPlayers
+	}, {
+		$push: { players: userId }
+	},
+	callbackIfSuccessful(successCallback));
+};
+
+GameStateManager.removePlayerFromGame = function(gameId, userId, successCallback) {
+	Games.update({
+		_id: gameId,
+		// The creator of a game cannot leave it
+		creator: { $ne: userId },
+		// Players cannot leave an in-progress game
+		status: Status.waitingForPlayers
+	}, {
+		$pull: { players: userId },
+		$unset: { dateReadyToBegin: '' }
+	},
+	callbackIfSuccessful(successCallback));
+};
+
+GameStateManager.tryMarkGameAsReady = function(gameId, successCallback) {
+	Games.update({
+		_id: gameId,
+		// Mark as ready only if status is still waitingForPlayers
+		// and the game is at full capacity
+		status: Status.waitingForPlayers,
+		$where: "this.players.length === this.playerCount"
+	}, {
+		$set: { dateReadyToBegin: new Date() }
+	},
+	callbackIfSuccessful(successCallback));
+};
+
+GameStateManager.beginGame = function(gameId, successCallback) {
 	Games.update({
 		_id: gameId,
 		// Begin only if status is still waitingForPlayers
@@ -34,7 +89,7 @@ function beginGame(gameId, successCallback) {
 	});
 };
 
-function setupNewRound(gameId, roundNum, successCallback) {
+GameStateManager.setupNewRound = function(gameId, roundNum, successCallback) {
 	var game = Games.findOne(gameId, { fields: { playerCount: 1 } });
 	var settings = Presets[game.playerCount].missions[roundNum];
 	if (!settings) { return; } // No such round (i.e., roundNum not in 1-5)
@@ -57,11 +112,11 @@ function setupNewRound(gameId, roundNum, successCallback) {
 		$where: "this.currentRound === 0 || this.rounds[this.currentRound].complete"
 	},
 	updates,
-	successCallback);
+	callbackIfSuccessful(successCallback));
 }
 
 /////////////////////////////////
-// Private service methods
+// Private methods
 /////////////////////////////////
 
 function prepareGame(gameId, successCallback) {
@@ -83,7 +138,17 @@ function prepareGame(gameId, successCallback) {
 			currentRound: 0
 		}
 	},
-	successCallback);
+	callbackIfSuccessful(successCallback));
 };
+
+function callbackIfSuccessful(successCallback) {
+	if (!successCallback) { return null; }
+	return function(err, updated) {
+		if (err || updated === 0) {
+			return;
+		}
+		successCallback.call();
+	};
+}
 
 });
